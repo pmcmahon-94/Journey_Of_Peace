29b63509f7bc1805e1d5003379531257ca03c305
Peter McMahon <pmcmahon12@qub.ac.uk>
2021-09-22 00:54:19 +0100

    Toolbar added to activity for navigation purposes - back button added to easily access MainActivityTest.kt

--- app/src/main/java/com/example/journeyofpeace/AboutActivity.kt
+++ app/src/main/java/com/example/journeyofpeace/AboutActivity.kt
@@ -2,10 +2,19 @@ package com.example.journeyofpeace
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class AboutActivity : AppCompatActivity() {
+
+    private lateinit var toolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
     }
 }
