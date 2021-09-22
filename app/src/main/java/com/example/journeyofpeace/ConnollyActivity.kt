679d188b7bf8136d84d40fb1ea60b3f721c88c83
Peter McMahon <pmcmahon12@qub.ac.uk>
2021-09-22 01:03:35 +0100

    Toolbar added for navigational purposes. MediaController controls the playing of the video and anchors controller to videoView

--- app/src/main/java/com/example/journeyofpeace/ConnollyActivity.kt
+++ app/src/main/java/com/example/journeyofpeace/ConnollyActivity.kt
@@ -10,14 +10,21 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ScrollView
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 
 class ConnollyActivity : AppCompatActivity() {
 
+    private lateinit var toolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_connolly)
+
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val urlString = "android.resource://com.example.journeyofpeace/" + R.raw.easter_rising
 
