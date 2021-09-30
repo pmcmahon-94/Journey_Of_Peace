6de28d5a1abbdf3ebc6624e3e0ce4470183aebc9
Peter McMahon <pmcmahon12@qub.ac.uk>
2021-09-30 15:05:01 +0100

    code tidy up and commented

--- app/src/main/java/com/example/journeyofpeace/LocationActivity.kt
+++ app/src/main/java/com/example/journeyofpeace/LocationActivity.kt
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
 
-class JourneyActivity : AppCompatActivity() {
+/**
+ * Gives access to the activity_location layout
+ */
+class LocationActivity : AppCompatActivity() {
 
     private lateinit var toolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_journey)
+        setContentView(R.layout.activity_location)
 
         //create toolbar
         toolbar = findViewById(R.id.toolbar)
