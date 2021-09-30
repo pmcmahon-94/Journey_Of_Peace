cdb936998530de79fed3a9f569a1e9e89e386ef2
Peter McMahon <pmcmahon12@qub.ac.uk>
2021-09-30 14:57:56 +0100

    Code tidy up and commente

--- app/src/androidTest/java/com/example/journeyofpeace/AboutActivityAndroidTest.kt
+++ app/src/androidTest/java/com/example/journeyofpeace/AboutActivityAndroidTest.kt
@@ -4,7 +4,6 @@ import androidx.test.espresso.Espresso
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.rules.ActivityScenarioRule
-import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
 
